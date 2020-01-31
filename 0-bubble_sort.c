b416d349ae2fd8152d41bb61e04c26490506f20e
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-31 20:18:57 +0100

    handle edge cases, optimize run time, complete 0-O and fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,37 +3,44 @@
 #include "sort.h"
 
 /**
-* swap - function that swaps two integers
-* @a: integer
-* @b: integer
-*/
+ * swap - function that swaps two integers
+ * @a: First integer
+ * @b: Second integer
+ */
 void swap(int *a, int *b)
 {
-int c;
+	int c;
 
-c = *a;
-*a = *b;
-*b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
 
 /**
-* bubble_sort - bubble sort algorithm
-* @array: array of integers
-* @size: size of array
-*/
+ * bubble_sort - bubble sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t i = 0, j = 0;
+	size_t i = 0, j = 0;
+	int swapped;
 
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-if (array[j] > array[j + 1])
-{
-swap(array + j, array + (j + 1));
-print_array(array, size);
-}
-}
-}
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array + j, array + (j + 1));
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
