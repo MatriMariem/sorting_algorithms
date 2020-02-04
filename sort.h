d33aec00ad6aecf21107fc385fda172f260c4cc0
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-02-04 17:25:02 +0100

    shell sort using knuth sequence

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void selection_sort(int *array, size_t size);
 int partition(int *array, int First, int Last, size_t size);
 void QS(int *array, int First, int Last, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
