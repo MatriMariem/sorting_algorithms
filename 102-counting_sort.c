4fcf846981cd9049146607440b2bdd6362daefc9
anaruzz <laabidigh@gmail.com>
2020-02-05 15:14:32 +0100

    betty fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,23 +1,28 @@
 #include "sort.h"
 #include <stdlib.h>
 
-
+/**
+* maximum - maximum of an array
+* @array: array of integers
+* @size: size of array
+* Return: integer
+*/
 int maximum(int *array, size_t size)
 {
-  size_t i;
-  int m = array[0];
-  for (i = 1; i < size; i++)
-  {
-    if (array[i] > m)
-    m = array[i];
-  }
-  return m;
+size_t i;
+int m = array[0];
+for (i = 1; i < size; i++)
+{
+if (array[i] > m)
+m = array[i];
+}
+return (m);
 }
 /**
- * bubble_sort - bubble sort algorithm
- * @array: array of integers
- * @size: size of the array
- */
+* counting_sort - counting sort algorithm
+* @array: array of integers
+* @size: size of the array
+*/
 void counting_sort(int *array, size_t size)
 {
 unsigned int max;
@@ -31,20 +36,20 @@ output = malloc(sizeof(array));
 for (i = 0; i <= max; i++)
 count[i] = 0;
 
-for(i = 0; i < size; i++)
-  {
-    val = array[i];
-    count[val] += 1;
-  }
+for (i = 0; i < size; i++)
+{
+val = array[i];
+count[val] += 1;
+}
 
-for(i = 1; i <= max; i++)
-  count[i] += count[i - 1];
+for (i = 1; i <= max; i++)
+count[i] += count[i - 1];
 print_array(count, max + 1);
 
 for (i = 0; i < size; i++)
 {
-  output[count[array[i]] - 1] = array[i];
-  count[array[i]]--;
+output[count[array[i]] - 1] = array[i];
+count[array[i]]--;
 }
 for (i = 0; i < size; i++)
 array[i] = output[i];
