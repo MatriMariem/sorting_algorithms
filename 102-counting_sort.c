d49a822a276230a5f0094fcd7a4318b7d5a23a67
anaruzz <laabidigh@gmail.com>
2020-02-05 15:28:38 +0100

    Counnting sort fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -35,23 +35,20 @@ return;
 max = maximum(array, size);
 count = malloc(sizeof(size_t) * (max + 1));
 output = malloc(sizeof(array));
-
 for (i = 0; i <= max; i++)
 count[i] = 0;
-
 for (i = 0; i < size; i++)
 {
 val = array[i];
 count[val] += 1;
 }
-
 for (i = 1; i <= max; i++)
 count[i] += count[i - 1];
 print_array(count, max + 1);
-
 for (i = 0; i < size; i++)
 {
-output[count[array[i]] - 1] = array[i];
+val = count[array[i]];
+output[val - 1] = array[i];
 count[array[i]]--;
 }
 for (i = 0; i < size; i++)
